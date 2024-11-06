78ea8ed549f7d962b7a4fb7d20a7f8e6a06cc558
jameskai <kai.hsiang.ju@gmail.com>
2024-11-06 09:50:44 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DataGraphConvert
-
+## Wrapper for the DataGraph R package to convert the output of julia DataFrames to DataGraph data structures, which can be read on-the-fly by DataGraph.
 [![Build Status](https://github.com/jameskai/DataGraphConvert.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/jameskai/DataGraphConvert.jl/actions/workflows/CI.yml?query=branch%3Amain)
--- src/DataGraphConvert.jl
+++ src/DataGraphConvert.jl
@@ -1,5 +1,34 @@
 module DataGraphConvert
 
-# Write your package code here.
+using DataFrames
+import RCall: RObject, @R_str
+# using PrecompileTools
+
+# Run when the module is loaded
+function __init__()
+    # Ensure DataGraph is installed in R's library
+    R"library(DataGraph)"
+end
+
+# DTable
+openDTable(path) = R"openDTable($path)"
+
+function writeDTable(path, df)
+    df_in_R = RObject(df)
+    R"writeDTable($path, $df_in_R)"
+end
+
+addDTable(path, df) = R"addDTable($path, $df)"
+syncDTable(path, df) = R"syncDTable($path, $df)"
+closeDTable(path) = R"closeDTable($path)"
+
+# DTBin
+openDTBin(path) = R"openDTBin($path)"
+addDTBin(path, df_name, df) = R"addDTBin($path, $df_name, $df)"
+syncDTBin(path) = R"syncDTBin($path)"
+closeDTBin(path) = R"closeDTBin($path)"
+
+# export function
+export openDTable, writeDTable, addDTable, syncDTable, closeDTable, openDTBin, addDTBin, closeDTBin, syncDTBin
 
 end
